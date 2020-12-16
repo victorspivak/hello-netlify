a63bf763585c1044bb66de34febafebd00f5341f
vspivak <victor.spivak@salesforce.com>
2020-12-15 23:43:52 -0800

    Updated function

--- src/lambda/hello-world.js
+++ src/lambda/hello-world.js
@@ -1,7 +1,8 @@
 exports.handler = function (event, context, callback) {     
   callback(null, {
     statusCode: 200,
-    body: 'Hello World <p>Victor'
+    body: 'Hello World
+      Victor'
   });
 };
 
